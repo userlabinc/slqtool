7f5c13285d52b4481585c1b35aa7955f17c3ef76
Moises Urias <moises.urias@userlab.co>
2020-04-05 02:06:38 -0600

    Remove whitespaces

--- backend/handler.js
+++ backend/handler.js
@@ -9,20 +9,15 @@ const ExcelJS = require("exceljs");
 module.exports.run = async event => {
     try{
         const group = verifyGroup(event)
-
         if (!group)
             throw Error('group_not_valid')
-
         if (event.body === null || event.body === undefined )
             throw Error('missing_params..')
-
         let body = JSON.parse(event.body)
-
         if(!body || body.query === '') throw Error('missing_body')
         const connection = await sql.connect(db(group))
         const db_response = await sql.query(body.query)
         return await response(200, db_response, connection)
-
     }catch (e) {
         console.log(e, '<--- error')
         return await response(400,e.message, null)
@@ -60,18 +55,14 @@ module.exports.details = async event => {
 
 module.exports.saveQuery = async event => {
     try{
-
         if (event.body === null || event.body === undefined )
             throw Error('missing_params ...')
 
         let body = JSON.parse(event.body)
-
         if(!body) throw Error('missing_body')
-
         const dataToS3 = serializeDataToS3(body)
         const S3 = new AWS.S3()
         await saveToS3(dataToS3, S3 )
-
         return await response(200, dataToS3, null)
     }catch (e) {
         console.log(e, '<--- error')
@@ -97,12 +88,9 @@ module.exports.excel = async (event) => {
         }
 
         let body = JSON.parse(event.body);
-
         if (!body || body.query === "") throw Error("missing_body");
-
         const connection = await sql.connect(db);
         const db_response = await sql.query(body.query);
-
         let workbook = new ExcelJS.Workbook();
         let creation_date = new Date();
         workbook.creator = "Moocho";
