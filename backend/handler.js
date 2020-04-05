aba9b6be8db83772fcc001ae6cfad235a3f6ebf9
Moises Urias <moises.urias@userlab.co>
2020-04-05 01:43:11 -0600

    It is possible to make a query, save the excel in the bucket and return the link to the user

--- backend/handler.js
+++ backend/handler.js
@@ -1,86 +1,151 @@
-'use strict';
-const sql = require('mssql')
-const { response, readTables, detailsTable, serializeDataToS3, saveToS3, getDataFromS3 } = require('./utils')
-const { db } = require('./db')
-const AWS = require('aws-sdk')
+"use strict";
+const sql = require("mssql");
+const {
+  response,
+  readTables,
+  detailsTable,
+  serializeDataToS3,
+  saveToS3,
+  saveExcelToS3,
+  getDataFromS3,
+} = require("./utils");
+const { db } = require("./db");
+const AWS = require("aws-sdk");
+const Stream = require("stream");
+const ExcelJS = require("exceljs");
 
-module.exports.run = async event => {
-    try{
-        if (event.body === null || event.body === undefined )
-            throw Error('missing_params')
-        
-        let body = JSON.parse(event.body)
-        
-        if(!body || body.query === '') throw Error('missing_body')
-        
-        const connection = await sql.connect(db)
-        const db_response = await sql.query(body.query)
-        
-        return await response(200, db_response, connection)
-        
-    }catch (e) {
-        console.log(e, '<--- error')
-        return await response(400,e.message, null)
+module.exports.run = async (event) => {
+  try {
+    if (event.body === null || event.body === undefined)
+      throw Error("missing_params");
+
+    let body = JSON.parse(event.body);
+
+    if (!body || body.query === "") throw Error("missing_body");
+
+    const connection = await sql.connect(db);
+    const db_response = await sql.query(body.query);
+
+    return await response(200, db_response, connection);
+  } catch (e) {
+    console.log(e, "<--- error");
+    return await response(400, e.message, null);
+  }
+};
+
+module.exports.tables = async (event) => {
+  try {
+    const connection = await sql.connect(db);
+    const db_response = await sql.query(readTables());
+    const tables = db_response.recordsets[0].map((x) => x.TABLE_NAME);
+    return await response(200, tables, connection);
+  } catch (e) {
+    console.log(e, "<--- error");
+    return await response(400, e.message, null);
+  }
+};
+
+module.exports.details = async (event) => {
+  try {
+    const param = event.queryStringParameters;
+
+    if (!param || !param.name) Error("missing_body");
+
+    const connection = await sql.connect(db);
+    const db_response = await sql.query(detailsTable(param));
+    const columns = db_response.recordsets[0].map(
+      (x) => `${x.COLUMN_NAME} (${x.DATA_TYPE})`
+    );
+    return await response(200, columns, connection);
+  } catch (e) {
+    console.log(e, "<--- error");
+    return await response(400, e.message, null);
+  }
+};
+
+module.exports.saveQuery = async (event) => {
+  try {
+    if (event.body === null || event.body === undefined)
+      throw Error("missing_params ...");
+
+    let body = JSON.parse(event.body);
+
+    if (!body) throw Error("missing_body");
+
+    const dataToS3 = serializeDataToS3(body);
+    const S3 = new AWS.S3();
+    await saveToS3(dataToS3, S3);
+
+    return await response(200, dataToS3, null);
+  } catch (e) {
+    console.log(e, "<--- error");
+    return await response(400, e.message, null);
+  }
+};
+
+module.exports.getQueries = async (event) => {
+  try {
+    const S3 = new AWS.S3();
+    const rp = await getDataFromS3(S3);
+    return await response(200, rp, null);
+  } catch (e) {
+    console.log(e, "<--- error");
+    return await response(400, e.message, null);
+  }
+};
+
+module.exports.excel = async (event) => {
+  try {
+    if (event.body === null || event.body === undefined)
+      throw Error("missing_params");
+
+    let body = JSON.parse(event.body);
+
+    if (!body || body.query === "") throw Error("missing_body");
+
+    const connection = await sql.connect(db);
+    const db_response = await sql.query(body.query);
+
+    let workbook = new ExcelJS.Workbook();
+    let creation_date = new Date();
+    workbook.creator = "Moocho";
+    workbook.modified = creation_date;
+    workbook.lastPrinted = creation_date;
+
+    let sheet = workbook.addWorksheet("My Sheet");
+    const stream = new Stream.PassThrough();
+
+    const { recordset } = db_response;
+    if (!(recordset && recordset[0])) {
+      throw Error("There is no recordset");
     }
-}
 
-module.exports.tables = async event => {
-    try{
-        const connection = await sql.connect(db)
-        const db_response = await sql.query(readTables())
-        const tables = db_response.recordsets[0].map( x => x.TABLE_NAME)
-        return await response(200, tables, connection)
-    }catch (e) {
-        console.log(e, '<--- error')
-        return await response(400,e.message, null)
-    }
-}
+    let first_reccord = recordset[0];
+    let recordset_keys = Object.keys(first_reccord);
 
-module.exports.details = async event => {
-    try{
-        const param = event.queryStringParameters
-        
-        if(!param || !param.name ) Error('missing_body')
-        
-        const connection = await sql.connect(db)
-        const db_response = await sql.query(detailsTable(param))
-        const columns = db_response.recordsets[0].map( x => `${x.COLUMN_NAME} (${x. DATA_TYPE})`)
-        return await response(200, columns, connection)
-    }catch (e) {
-        console.log(e, '<--- error')
-        return await response(400,e.message, null)
-    }
-}
+    sheet.columns = recordset_keys.map((element) => ({
+      header: element,
+      key: element,
+    }));
 
-module.exports.saveQuery = async event => {
-    try{
-        
-        if (event.body === null || event.body === undefined )
-            throw Error('missing_params ...')
-    
-        let body = JSON.parse(event.body)
-    
-        if(!body) throw Error('missing_body')
-        
-        const dataToS3 = serializeDataToS3(body)
-        const S3 = new AWS.S3()
-        await saveToS3(dataToS3, S3 )
-        
-        return await response(200, dataToS3, null)
-    }catch (e) {
-        console.log(e, '<--- error')
-        return await response(400,e.message, null)
-    }
-}
+    recordset.forEach((element) => {
+      sheet.addRow(element);
+    });
+    const S3 = new AWS.S3();
+    const random = Math.floor(Math.random() * 100);
 
-module.exports.getQueries = async event => {
-    try{
-        const S3 = new AWS.S3()
-        const rp = await getDataFromS3(S3)
-        return await response(200, rp, null)
-    }catch (e) {
-        console.log(e, '<--- error')
-        return await response(400,e.message, null)
-    }
-}
+    const key = `recordset_${new Date().getTime()}_${random}.xlsx`;
+    let download_link = "";
+    await workbook.xlsx.write(stream);
+    await saveExcelToS3(stream, key, S3);
+    download_link = S3.getSignedUrl("getObject", {
+      Key: key,
+      Bucket: process.env.BUCKET,
+    });
 
+    return await response(200, { link: download_link }, connection);
+  } catch (e) {
+    console.log(e, "<--- error");
+    return await response(400, e.message, null);
+  }
+};
--- backend/utils.js
+++ backend/utils.js
@@ -1,71 +1,94 @@
-const readTables = () => `SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='Moocho' ORDER by TABLE_NAME ASC;`
+const readTables = () =>
+  `SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='Moocho' ORDER by TABLE_NAME ASC;`;
 
-const detailsTable = p => `select COLUMN_NAME, DATA_TYPE from information_schema.columns where table_name = '${p.name}';`
+const detailsTable = (p) =>
+  `select COLUMN_NAME, DATA_TYPE from information_schema.columns where table_name = '${p.name}';`;
 
 const serializeDataToS3 = (data) => {
-  let params = { queries:data }
-  return params
-}
+  let params = { queries: data };
+  return params;
+};
 
-const saveToS3 = async (dataToS3,S3 ) => {
-  
+const saveToS3 = async (dataToS3, S3) => {
   const paramsToS3 = {
     Body: JSON.stringify(dataToS3),
     Bucket: process.env.BUCKET,
     Key: `store.json`,
-    Tagging: new Date().toISOString()
-  }
-  
+    Tagging: new Date().toISOString(),
+  };
+
   const P = await new Promise((resolve, reject) => {
-    S3.putObject(paramsToS3, function(err, data) {
+    S3.putObject(paramsToS3, function (err, data) {
       if (err) {
-        console.log(err)
-        reject(err) // an error occurred
+        console.log(err);
+        reject(err); // an error occurred
       } else {
-        console.log(data)
-        resolve(data)
+        console.log(data);
+        resolve(data);
       }
-    })
-  })
-  
-  return P
-  
-}
+    });
+  });
+
+  return P;
+};
+
+const saveExcelToS3 = async (stream, key, S3) => {
+  const paramsToS3 = {
+    Body: stream,
+    Bucket: process.env.BUCKET,
+    Key: key,
+    Tagging: new Date().toISOString(),
+    ContentType:
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  };
+
+  const P = await new Promise((resolve, reject) => {
+    S3.upload(paramsToS3, function (err, data) {
+      if (err) {
+        console.log(err);
+        reject(err); // an error occurred
+      } else {
+        resolve(data);
+      }
+    });
+  });
+
+  return P;
+};
 
 const getDataFromS3 = async (S3) => {
   const params = {
     Bucket: process.env.BUCKET,
-    Key: 'store.json'
+    Key: "store.json",
   };
-  
-  let p = await new Promise(((resolve, reject) => {
-     S3.getObject(params, function(err, data) {
-      if (err){
-        console.log(err)
+
+  let p = await new Promise((resolve, reject) => {
+    S3.getObject(params, function (err, data) {
+      if (err) {
+        console.log(err);
         reject(err); // an error occurred
-      }
-      else{
-         resolve(data.Body.toString())
+      } else {
+        resolve(data.Body.toString());
       }
     });
-  }))
-  
-  return JSON.parse(p)
-}
+  });
+
+  return JSON.parse(p);
+};
 
 const response = (status, body, connection) => {
   if (connection) connection.close();
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     resolve({
       statusCode: status,
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
       },
       body: JSON.stringify(body),
     });
   });
-}
+};
 
 module.exports = {
   response,
@@ -73,5 +96,6 @@ module.exports = {
   detailsTable,
   serializeDataToS3,
   saveToS3,
-  getDataFromS3
-}
+  getDataFromS3,
+  saveExcelToS3,
+};
