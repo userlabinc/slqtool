8bc5a0ab1750f7c06b8d0cfbd9e2006e1b40ab5c
cesar <cesar.augs@gmail.com>
2020-06-12 10:03:42 -0600

    -fix copy to clipBoard function

--- frontend/src/views/queries/QueryPage.js
+++ frontend/src/views/queries/QueryPage.js
@@ -121,18 +121,9 @@ const QueryPage = props => {
   }
 
   const copyToClipBoard = () => {
-    if (
-      document.querySelector(
-        '.ant-table-container .ant-table-content table tbody tr td .ant-empty.ant-empty-normal'
-      ) != null
-    ) {
-      return message.error('There is no data to copy')
-    }
-    CopyToClipboardFromTableBody(
-      document.querySelector(
-        '.ant-table-container .ant-table-content table tbody'
-      )
-    )
+    let tableToCopy = document.querySelector('.dynamic-table-2 .ant-table .ant-table-container .ant-table-content table')
+    if(tableToCopy === null ) return message.error('There is no data to copy')
+    CopyToClipboardFromTableBody(tableToCopy)
   }
 
   return (
