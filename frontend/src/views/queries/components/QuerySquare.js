c8a6f7a01a326e2e141c762f8e374e85528741d6
cesar <cesar.augs@gmail.com>
2020-06-15 15:46:33 -0600

    - refactor query area page

--- frontend/src/views/queries/components/QuerySquare.js
+++ frontend/src/views/queries/components/QuerySquare.js
@@ -1,15 +1,52 @@
-import React from 'react'
-import { Input } from 'antd'
+import React, {useEffect, useState} from 'react'
+import {Button, Col, Divider, Input } from 'antd'
+import CustomVerticalDivider from "./CustomVerticalDivider";
+
 
 const QuerySquare = props => {
   const { TextArea } = Input
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    setQuery(props.querySaved)
+    // eslint-disable-next-line
+  }, [props.querySaved])
+
   return (
+    <>
     <TextArea
       disabled={props.loading}
       className={'query-square ' + props.className}
-      onChange={event => props.setQuery(event.target.value)}
-      value={props.query}
+      value={query}
+      name={'query'}
+      onChange={event => setQuery(event.target.value) }
     />
+      <Divider style={{ backgroundColor: 'lightgray' }} />
+      <Col sm={24} style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button disabled={props.loading} onClick={_=>props.handleQuery(query)}  type={'primary'}>
+           Execute
+        </Button>
+        <CustomVerticalDivider />
+        <Button
+          disabled={props.loading}
+          //onClick={() => setIsOpenSaveQueryModal(true)}
+        >
+          Save Query
+        </Button>
+        <CustomVerticalDivider />
+
+        <Button disabled={props.loading} onClick={_=>props.handlerExcel(query)}  >
+          Execute to Excel
+        </Button>
+
+        <CustomVerticalDivider />
+        <Button
+          disabled={props.loading} onClick={props.handleToCopy} >
+          Copy to ClipBoard
+        </Button>
+
+      </Col>
+    </>
   )
 }
 
